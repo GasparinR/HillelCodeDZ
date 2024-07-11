8a8c142d61a50140845aa493f6ba3ec52111aaac
GasparinR <mrgasparin@ukr.net>
2024-07-11 10:53:44 +0300

    4.3u

--- dzcode/dz4.1.py
+++ dzcode/dz4.1.py
@@ -1,3 +1,5 @@
-the_list = [0, 1, 0, 6, 0, 3, 1, 0, 9, 0]
-the_list.sort(key=bool, reverse=True)
-print(the_list)
+the_list = [0, 0, 1, 0.5, 0, -6, 0, 3, 1.1, 0.1, 9, 0]
+a = [x for x in the_list if x != 0]
+b = [x for x in the_list if x == 0]
+result = a + b
+print(result)
