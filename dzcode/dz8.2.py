4a359d0062f0a909e66fdf4286c61f99f0f94e8f
GasparinR <mrgasparin@ukr.net>
2024-07-29 10:37:23 +0300

    8.2 dz

--- dzcode/dz8.2.py
+++ dzcode/dz8.2.py
@@ -1,16 +1,11 @@
 def is_palindrome(text):
-    # Приводим строку к нижнему регистру
     text = text.lower()
-    # Оставляем только буквы и цифры
     text = ''.join(char for char in text if char.isalnum())
-    # Проверяем, является ли строка палиндромом
     result = text == text[::-1]
-    # Выводим результат проверки
     print(f'Is the text "{text}" a palindrome? {result}')
     return result
 
 
-# Тесты
 is_palindrome('A man, a plan, a canal: Panama')
 is_palindrome('0P')
 is_palindrome('a.')
