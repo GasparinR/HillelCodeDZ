489fbe33a8ccc2cf8f027462508e45dcc59326c8
GasparinR <mrgasparin@ukr.net>
2024-08-16 08:31:25 +0300

    dz15.2

--- dzcode/dz15.2.py
+++ dzcode/dz15.2.py
@@ -10,43 +10,35 @@ class Fraction:
         self.simplify()
 
     def simplify(self):
-        # Simplify the fraction to its simplest form
         common_divisor = gcd(self.a, self.b)
         self.a //= common_divisor
         self.b //= common_divisor
 
     def __mul__(self, other):
-        # Multiplication of fractions: (a/b) * (c/d) = (a*c) / (b*d)
         new_a = self.a * other.a
         new_b = self.b * other.b
         return Fraction(new_a, new_b)
 
     def __add__(self, other):
-        # Addition of fractions: (a/b) + (c/d) = (a*d + b*c) / (b*d)
         new_a = self.a * other.b + self.b * other.a
         new_b = self.b * other.b
         return Fraction(new_a, new_b)
 
     def __sub__(self, other):
-        # Subtraction of fractions: (a/b) - (c/d) = (a*d - b*c) / (b*d)
         new_a = self.a * other.b - self.b * other.a
         new_b = self.b * other.b
         return Fraction(new_a, new_b)
 
     def __eq__(self, other):
-        # Check if two fractions are equal: (a/b) == (c/d) if a*d == b*c
         return self.a * other.b == self.b * other.a
 
     def __gt__(self, other):
-        # Greater than comparison: (a/b) > (c/d) if a*d > b*c
         return self.a * other.b > self.b * other.a
 
     def __lt__(self, other):
-        # Less than comparison: (a/b) < (c/d) if a*d < b*c
         return self.a * other.b < self.b * other.a
 
     def __str__(self):
-        # String representation of the fraction
         return f"Fraction: {self.a}, {self.b}"
 
 
